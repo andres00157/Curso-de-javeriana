5415a5e205534184774859f1692c2732ac04c5d5
andres00157 <andresfelipebarretojimenez@gmail.com>
2016-04-16 14:43:48 -0500

    Added files via upload

---
+++ 4.py
@@ -0,0 +1,69 @@
+print ("suma de los digitos de un numero")
+print ("de cuantos digitos quere trabajar")
+a = int(raw_input("numero de dijitos"))
+if a == 2 :
+    print("escribe el numero")
+    b = int(raw_input("numero="))
+    c = b/10
+    d = b%10
+    print (c + d )
+if a == 3 :
+    print("escribe el numero")
+    b = int(raw_input("numero="))
+    c = b/10
+    d = b%10
+    p = c/10
+    q = c%10
+    print (q + d + p )
+if a == 4 :
+    print("escribe el numero")
+    b = int(raw_input("numero="))
+    c = b/10
+    d = b%10
+    p = c/10
+    q = c%10
+    u = p / 10
+    o = p % 10
+    print (q + d + u + o )      
+if a == 5 :
+    print("escribe el numero")
+    b = int(raw_input("numero="))
+    c = b/10
+    d = b%10
+    p = c/10
+    q = c%10
+    u = p / 10
+    o = p % 10
+    i = u/10
+    e = u%10
+    print (q + d + o + i + e ) 
+if a == 6 :
+    print("escribe el numero")
+    b = int(raw_input("numero="))
+    c = b/10
+    d = b%10
+    p = c/10
+    q = c%10
+    u = p / 10
+    o = p % 10
+    i = u/10
+    e = u%10
+    m = i/10
+    n = i%10
+    print (q + d + o + e + m + n ) 
+if a == 7 :
+    print("escribe el numero")
+    b = int(raw_input("numero="))
+    c = b/10
+    d = b%10
+    p = c/10
+    q = c%10
+    u = p / 10
+    o = p % 10
+    i = u/10
+    e = u%10
+    m = i/10
+    n = i%10
+    l = m/10
+    j = m%10 
+    print (q + d + o + e + n + j + l ) 
